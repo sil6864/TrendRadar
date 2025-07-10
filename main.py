6e84012247c5ff697a5078bbcfe64d3e8bdcfcdf
sil6864 <156509779+sil6864@users.noreply.github.com>
2025-07-10 10:32:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2589,13 +2589,13 @@ class NewsAnalyzer:
             ("baidu", "百度热搜"),
             ("wallstreetcn-hot", "华尔街见闻"),
             ("thepaper", "澎湃新闻"),
-            ("bilibili-hot-search", "bilibili 热搜"),
-            ("cls-hot", "财联社热门"),
+            # ("bilibili-hot-search", "bilibili 热搜"),
+            # ("cls-hot", "财联社热门"),
             ("ifeng", "凤凰网"),
             "tieba",
-            "weibo",
-            "douyin",
-            "zhihu",
+            # "weibo",
+            # "douyin",
+            # "zhihu",
         ]
 
         print(f"开始爬取数据，请求间隔 {self.request_interval} 毫秒")
